21f6507805e070d67d3ee5e322b4a4386d6e2db4
Scar <scarabei333ruslan@gmail.com>
2024-04-24 00:53:19 +0300

    Version 2.0

--- _logger.py
+++ _logger.py
@@ -4,14 +4,24 @@ import os
 
 
 class CustomLogger():
-    
+
     def __init__(self, path_to_folder: str = None) -> None:
+        self.name_folder = 'Log'
+        if path_to_folder:
+            self.path_to_folder = os.path.join(path_to_folder, self.name_folder)
+            self._check_folder_for_log_file()
+            self.file_path = os.path.join(self.path_to_folder, f'{datetime.now().strftime("%Y.%m.%d")}.log')
+        else:
+            self.path_to_folder = os.path.join(os.getcwd(), self.name_folder)
+            self._check_folder_for_log_file()
+            self.file_path = os.path.join(self.path_to_folder, f'{datetime.now().strftime("%Y.%m.%d")}.log')
+
         self.log = logging.getLogger(__name__)
         self.log.setLevel(logging.DEBUG)
 
-        self.formatter = logging.Formatter('f"%(asctime)s - [%(levelname)s] - [%(filename)s] - %(funcName)s: (%(lineno)d) - %(message)s"')
+        self.formatter = logging.Formatter('%(asctime)s - [%(levelname)s] - [%(filename)s] - %(funcName)s: (%(lineno)d) - %(message)s')
 
-        self.file_handler = logging.FileHandler(f'{datetime.now().strftime('%Y.%m.%d')}.log')
+        self.file_handler = logging.FileHandler(self.file_path)
         self.file_handler.setLevel(logging.DEBUG)
         self.file_handler.setFormatter(self.formatter)
 
@@ -22,18 +32,12 @@ class CustomLogger():
         self.log.addHandler(self.file_handler)
         self.log.addHandler(self.console_handler)
 
-        self.name_folder = 'Log'
-        if path_to_folder:
-            self.path_to_folder = os.path.join(path_to_folder, self.name_folder)
-        else: 
-            self.path_to_folder = os.path.join(os.getcwd(), self.name_folder)
-    
     def _check_folder_for_log_file(self) -> None:
         if not os.path.exists(self.path_to_folder):
             os.mkdir(self.path_to_folder)
+        
 
     def start_initialization(self):
-        self._check_folder_for_log_file()
+        return self.log
 
-
-log = CustomLogger(path_to_folder=r'D:\Code\Python\Python_Logger\Log').start_initialization()
+log = CustomLogger(path_to_folder=r'path_to_folder').start_initialization()
